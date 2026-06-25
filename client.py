2810f78a0e75e1410ab46eb569d93227717033e3
volkozaq <volkozaq@gmail.com>
2026-06-25 23:09:35 +0300

    Fix1

--- client.py
+++ client.py
@@ -93,28 +93,28 @@ BASE_URL = "http://localhost:8000"
 #     json={"username": "testuser2", "password": "1234"}
 # )
 # print(f'Create user 4 status: {create_response.status_code}')
-resp = requests.post(
-    f'{BASE_URL}/login',
-    json={"username": "testuser7", "password": "4567"}
-)
-print(resp.json())
-user1_token = resp.json()['token']
-user1_id = resp.json()['user_id']
-print(user1_id)
-
-resp = requests.post(
-    f'{BASE_URL}/advertisement',
-    json={"title": "Test advert", "description": "Test description","author_id": user1_id, "price": 2},
-    headers={"x-token": user1_token}
-)
-print(resp.json())
-ad_id = resp.json()['id']
-
-resp = requests.get(
-    f'{BASE_URL}/advertisement/{ad_id}',
-
-)
-print(resp.json())
+# resp = requests.post(
+#     f'{BASE_URL}/login',
+#     json={"username": "testuser7", "password": "4567"}
+# )
+# print(resp.json())
+# user1_token = resp.json()['token']
+# user1_id = resp.json()['user_id']
+# print(user1_id)
+# 
+# resp = requests.post(
+#     f'{BASE_URL}/advertisement',
+#     json={"title": "Test advert", "description": "Test description","author_id": user1_id, "price": 2},
+#     headers={"x-token": user1_token}
+# )
+# print(resp.json())
+# ad_id = resp.json()['id']
+# 
+# resp = requests.get(
+#     f'{BASE_URL}/advertisement/{ad_id}',
+# 
+# )
+# print(resp.json())
 
 resp = requests.post(
     f'{BASE_URL}/login',
@@ -124,72 +124,73 @@ print(resp.json())
 new_tok = resp.json()['token']
 new_id = resp.json()['user_id']
 
-print(f'admin:')
+print(f'admin: {new_id}')
 resp = requests.get(
-    f'{BASE_URL}/user/{new_id}'
-)
-print(resp.json())
-
-resp = requests.patch(
-    f'{BASE_URL}/advertisement/{ad_id}',
-    json={"title": "New advert"},
+    f'{BASE_URL}/user',
     headers={"x-token": new_tok}
 )
 print(resp.json())
 
-print(f'New user tries to update user1')
-resp = requests.get(
-    f'{BASE_URL}/advertisement/{ad_id}',
-
-)
-print(resp.json())
-
-resp = requests.delete(
-    f'{BASE_URL}/advertisement/{ad_id}',
-    headers={"x-token": new_tok}
-)
-print(resp.json())
-
-
-resp = requests.get(
-    f'{BASE_URL}/advertisement/{ad_id}',
-
-)
-print(resp.json())
-
-
-print(f'Admin tries to update user1')
-resp = requests.patch(
-    f'{BASE_URL}/user/{user1_id}',
-    json={"username": "testuser7"},
-    headers={"x-token": new_tok}
-)
-print(resp.json())
-
-print(f'Updated user:')
-resp = requests.get(
-    f'{BASE_URL}/user/{user1_id}'
-)
-print(resp.json())
-
-print("Admin tries to delete")
-resp = requests.delete(
-    f'{BASE_URL}/user/{user1_id}',
-    headers={"x-token": new_tok}
-)
-print(f'Status: {resp.status_code}')
-if resp.status_code == 200:
-    user_data = resp.json()
-    print(user_data)
-else:
-    print(f'Error: {resp.text}')
-print(f'Status: {resp.status_code}')
-
-print(f'Updated user:')
-resp = requests.get(
-    f'{BASE_URL}/user/{user1_id}'
-)
-print(resp.json())
+# resp = requests.patch(
+#     f'{BASE_URL}/advertisement/{ad_id}',
+#     json={"title": "New advert"},
+#     headers={"x-token": new_tok}
+# )
+# print(resp.json())
+# 
+# print(f'New user tries to update user1')
+# resp = requests.get(
+#     f'{BASE_URL}/advertisement/{ad_id}',
+# 
+# )
+# print(resp.json())
+# 
+# resp = requests.delete(
+#     f'{BASE_URL}/advertisement/{ad_id}',
+#     headers={"x-token": new_tok}
+# )
+# print(resp.json())
+# 
+# 
+# resp = requests.get(
+#     f'{BASE_URL}/advertisement/{ad_id}',
+# 
+# )
+# print(resp.json())
+# 
+# 
+# print(f'Admin tries to update user1')
+# resp = requests.patch(
+#     f'{BASE_URL}/user/{user1_id}',
+#     json={"username": "testuser7"},
+#     headers={"x-token": new_tok}
+# )
+# print(resp.json())
+# 
+# print(f'Updated user:')
+# resp = requests.get(
+#     f'{BASE_URL}/user/{user1_id}'
+# )
+# print(resp.json())
+# 
+# print("Admin tries to delete")
+# resp = requests.delete(
+#     f'{BASE_URL}/user/{user1_id}',
+#     headers={"x-token": new_tok}
+# )
+# print(f'Status: {resp.status_code}')
+# if resp.status_code == 200:
+#     user_data = resp.json()
+#     print(user_data)
+# else:
+#     print(f'Error: {resp.text}')
+# print(f'Status: {resp.status_code}')
+# 
+# print(f'Updated user:')
+# resp = requests.get(
+#     f'{BASE_URL}/user/{user1_id}'
+# )
+# print(resp.json())
 
 # print("\n--- Getting Ad by fields---")
 # response = requests.get(
